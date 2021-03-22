5e213551d251f51864ad63f46e27f8fdbc270397
zakruvalcaba <zakruvalcaba@gmail.com>
2021-03-21 19:54:29 -0700

    updated lab and updated to ES6

--- js/script.js
+++ js/script.js
@@ -1,66 +1,64 @@
-/*eslint-env browser*/
-
-//GLOBAL VARIABLES
-var i;          //Incrementer for loop
-var h2;         //Store the currently selected h2
-var div;        //Store the currently selected div
-var h2Elements; //Store the collection of h2 elements
-var faqs;       //Store the main tag
-
-//HELPER FUNCTION GET ELEMENTS FROM THE DOM
-var $ = function (id) {
-    "use strict";
-    return window.document.getElementById(id);
+const $ = (id) => {
+    return document.getElementById(id);
 };
 
-//MAIN FUNCTION TO TOGGLE VISIBILITY OF ACCORDION
-var toggle = function (e) {
-    "use strict";
-    //Get the currently selected h2 element
-    h2 = e.currentTarget;
-    //Get the div element for the currently selected element
-    div = h2.nextElementSibling;
-    //Get all h2 elements in the main tag
-    h2Elements = faqs.getElementsByTagName("h2");
-    //Loop through the collection of h2 elements
-    for (i = 0; i < h2Elements.length; i += 1) {
-        //If the h2 element is not the currently selected h2
-        if (h2Elements[i] !== e.currentTarget) {
-            //Remove the class attribute from the h2
-            h2Elements[i].removeAttribute("class");
-            //Remove the class attribute from the div
-            h2Elements[i].nextElementSibling.removeAttribute("class");
-        }
+const processEntries = () => {
+    let header = '';
+    let html = '';
+    let required = '<span>Required field</span>';
+    let msg = 'Please review your entries and complete all required fields';
+    let email = $('email_address').value;
+    let phone = $('phone').value;
+    let country = $('country').value;
+    let contact = 'Text';
+    if ($('email').checked) {
+        contact = 'Email';
+    }
+    if ($('none').checked) {
+        contact = 'None';
+    }
+    let terms = $('terms').checked;
+
+    if (email === '') {
+        email = required;
+        header = msg;
+    }
+	if (phone === '') {
+        phone = required;
+        header = msg;
+    }
+	if (country === '') {
+        country = required;
+        header = msg;
+    }
+    if (terms === false) {
+        terms = required;
+        header = msg;
     }
     
-    //If the currently selected h2 has a class attribute
-    if (h2.hasAttribute("class")) {
-        //Remove it
-        h2.removeAttribute("class");
+    $('registration_header').innerHTML = header;
+    if (header === msg) {
+        html = html + '<tr><td>Email:</td><td>' + email + '</td></tr>';
+        html = html + '<tr><td>Phone:</td><td>' + phone + '</td></tr>';
+        html = html + '<tr><td>Country:</td><td>' + country + '</td></tr>';
+        html = html + '<tr><td>Contact:</td><td>' + contact + '</td></tr>';
+        html = html + '<tr><td>Terms:</td><td>' + terms + '</td></tr>';
+        $('registration_info').innerHTML = html;
     } else {
-        //Otherwise set the class to .minus
-        h2.setAttribute("class", "minus");
-    }
-    //If the currently selected div has a class attribute
-    if (div.hasAttribute("class")) {
-        //Remove it
-        div.removeAttribute("class");
-    } else {
-        //Otherwise set the class to .open
-        div.setAttribute("class", "open");
+        $('registration_info').innerHTML = '';
+        $('registration_form').submit();
     }
 };
 
-//HANDLE PAGE LOAD
-window.addEventListener("load", function () {
-    "use strict";
-    //Get the main tag
-    faqs = $("faqs");
-    //Get all h2 elements in the main tag
-    h2Elements = faqs.getElementsByTagName("h2");
-    //Loop through the collection of h2 elements
-    for (i = 0; i < h2Elements.length; i += 1) {
-        //Add the click event to each h2 element
-        h2Elements[i].addEventListener("click", toggle);
-    }
+const resetForm = () => {
+    $('registration_form').reset();
+    $('registration_header').innerHTML = '';
+    $('registration_info').innerHTML = '';
+    $('email_address').focus();
+};
+
+window.addEventListener('load', () => {
+    $('register').addEventListener('click', processEntries);
+    $('reset_form').addEventListener('click', resetForm);
+    $('email_address').focus();
 });
